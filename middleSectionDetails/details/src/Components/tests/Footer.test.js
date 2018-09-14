0da67c4c5050a473690b5ed58d94fa4602f41bbd
Kenneth Oh <ohtkenneth@gmail.com>
2018-09-13 18:41:11 -0700

    Add additional jest tests

---
+++ middleSectionDetails/details/src/Components/tests/Footer.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Footer from '../Footer';
+import KeyWord from '../KeyWord';
+
+const testMovieData =  require('../../db/mockdata/mockMovieData.json')[0];
