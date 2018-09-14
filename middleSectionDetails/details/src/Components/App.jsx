2b918d011cc4c4299095e7e8fa51517d9dc73a49
Kenneth Oh <ohtkenneth@gmail.com>
2018-09-14 10:02:26 -0700

    Add addtional jest tests
    /Components meets minimum 65% statements code coverage. Next reach parity code coverage % for /server.

--- middleSectionDetails/details/src/Components/App.jsx
+++ middleSectionDetails/details/src/Components/App.jsx
@@ -18,7 +18,7 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
-      mainMovie: null,
+      currentMovie: null,
     };
   }
   componentDidMount() {
--- middleSectionDetails/details/src/Components/tests/Footer.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-
-import Footer from '../Footer';
-import KeyWord from '../KeyWord';
-
-const testMovieData =  require('../../db/mockdata/mockMovieData.json')[0];
