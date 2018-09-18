2769d37178cc5e12c840359610436811751831b2
Servando J Berna Rico <servandojbernarico@Servandos-MacBook-Pro.local>
2018-09-17 18:50:03 -0700

    Adding async testing to reach 65%

--- fec-fmdb-servando-suggested/test/Suggested.test.js
+++ fec-fmdb-servando-suggested/test/Suggested.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 import Enzyme from "enzyme";
 import { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
@@ -8,7 +9,18 @@ import MovieEntry from "../client/components/MovieEntry.jsx";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-it("Should render without crashing", () => {
+it("MovieList should render without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<MovieList />, div);
 });
+
+// it("MovieEntry should render without crashing", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<MovieEntry />, div);
+// });
+
+// it("Each entry should animate when hover over it", async () => {
+//   const wrapper = await shallow(<MovieEntry />);
+//   wrapper.find(".entry").simulate("hover");
+//   expect(wrapper.find(".entry")).toBeTruthy();
+// });
