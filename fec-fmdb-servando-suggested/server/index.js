ae3b20a1d11f973c05756804cfab52b3a3e022a6
Servando J Berna Rico <servandojbernarico@Servandos-MacBook-Pro.local>
2018-09-18 17:39:32 -0700

    Cors added.

--- fec-fmdb-servando-suggested/server/index.js
+++ fec-fmdb-servando-suggested/server/index.js
@@ -5,10 +5,12 @@ const db = require("../database/index.js");
 const PORT = 8080;
 const router = require("./routes.js");
 const app = express();
+const cors = require("cors");
 
 app.use(express.static(path.resolve(__dirname, "../static")));
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/movie", router);
 
